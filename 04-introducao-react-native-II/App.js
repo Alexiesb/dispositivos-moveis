05f3e29dee280c50b2d1d89b22557c052d2e5726
Alexiesb <alexandre.mendonca@iesb.edu.br>
2025-03-20 20:39:53 -0300

    Aula 20/03

--- 04-introducao-react-native-II/App.js
+++ 04-introducao-react-native-II/App.js
@@ -1,29 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>U neymá</Text>
-      <Image 
-      source={{
-        uri:'https://media1.tenor.com/m/fyzB1ihZTpIAAAAC/meme-an%C3%A3o.gif'}}
+      <View style={styles.container}>
+        <Text>TESTE</Text>
+      </View>
+  )
+}  
 
-      style={{
-        height: 400,
-        width: 200
-        }}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#1E66E2',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
+  })
