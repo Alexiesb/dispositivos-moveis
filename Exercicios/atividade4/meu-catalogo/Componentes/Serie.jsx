29d3c0ea163e122ccf5bf64793cfe67e57ce200f
Alexiesb <alexandre.mendonca@iesb.edu.br>
2025-03-28 00:26:50 +0000

    27/03

---
+++ Exercicios/atividade4/meu-catalogo/Componentes/Serie.jsx
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Serie(props) {
+  const {nome, ano, diretor, temporadas, capa} = props
+  return (
+    <View>
+      <Text style={styles.texto}>Filme</Text>
+      <Text style={styles.texto}>Nome: {nome}</Text>
+      <Text style={styles.texto}>Ano: {ano}</Text> 
+      <Text style={styles.texto}>Diretor: {diretor}</Text> 
+      <Text style={styles.texto}>Temporadas: {temporadas}</Text> 
+      
+      <Image
+       source={{
+        uri: capa
+        }}
+        style={{
+        height: 200,
+        width: 200
+        }}      
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'yellow',
+        borderWidth: 10,
+        padding: 5,
+        alignItems: 'center',
+        flex: 1
+      },
+    texto: {
+        fontSize: 20,
+        fontWeight: 600
+    }
+})
