d0951a28354491fc9abe819d0504bf7192d510c7
Alexiesb <alexandre.mendonca@iesb.edu.br>
2025-04-25 00:18:19 +0000

    24/04

---
+++ Exercicios/atividade6/receitas-app/App.js
@@ -0,0 +1,24 @@
+import {PaperProvider} from 'react-native-paper'
+import {NavigationContainer} from '@react-navigation/native'
+import StackRoutes from './src/routes/Stackroutes';
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        
+        <StackRoutes/>
+                          
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
